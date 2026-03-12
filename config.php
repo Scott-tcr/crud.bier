a0c67897019122b3071f4db9b9a12cdc4fbf0bc1
Scott-tcr <9028222@student.tcrmbo.nl>
2026-03-12 12:52:13 +0100

    stap 3

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // auteur: Scott Hameeteman
 // functie: configuratiebestand
@@ -10,17 +9,4 @@ define("PASSWORD", "");
 
 define("CRUD_TABLE", "bier");
 
-=======
-<?php
-// auteur: Vul hier je naam in
-// functie: configuratiebestand
-
-define("DATABASE", "fietsenmaker");
-define("SERVERNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-
-define("CRUD_TABLE", "fietsen");
-
->>>>>>> 8616bbad088c16b49022f6e03f95e8a6b4c44169
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -27,33 +26,3 @@
 
 
 
-=======
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-    <?php
-    // functie: Programma CRUD fietsen
-    // auteur: Vul hier je naam in   
-
-    // Initialisatie
-    include 'functions.php';
-
-    // Main
-
-    // Aanroep functie 
-    crudMain();
-    ?>
-
-</body>
-</html>
-
-
-
->>>>>>> 8616bbad088c16b49022f6e03f95e8a6b4c44169
